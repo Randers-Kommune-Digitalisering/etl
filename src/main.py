381bb45f69e3e4f1f3b1d32eabb21420873d8b9d
Rune Aagaard Keena <rune.aagaard.keena@randers.dk>
2024-05-27 16:30:59 +0200

    endpoints and fixes

--- src/main.py
+++ src/main.py
@@ -4,6 +4,7 @@ from prometheus_client import generate_latest
 
 from utils.logging import set_logging_configuration, APP_RUNNING
 from utils.config import DEBUG, PORT, POD_NAME
+from job_api import job_api_bp
 
 
 def create_app():
@@ -11,6 +12,7 @@ def create_app():
     health = HealthCheck()
     app.add_url_rule("/healthz", "healthcheck", view_func=lambda: health.run())
     app.add_url_rule('/metrics', "metrics", view_func=generate_latest)
+    app.register_blueprint(job_api_bp)
     APP_RUNNING.labels(POD_NAME).set(1)
     return app
 
