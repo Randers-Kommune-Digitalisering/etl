77984bebb89676289ecf01974901d6e29610cc31
Rune Aagaard Keena <rune.aagaard.keena@randers.dk>
2024-08-12 11:35:20 +0200

    test

--- src/jobs/test.py
+++ src/jobs/test.py
@@ -4,5 +4,10 @@ logger = logging.getLogger(__name__)
 
 def job():
     logging.info("Test job")
-    logger.info(requests.get("https://api.statbank.dk/v1/subjects"))
+    try:
+        res  = requests.get("https://api.statbank.dk/v1/subjects")
+        logger.info(res)
+    except Exception as e:
+        logger.error(e)
+        return False
     return True
