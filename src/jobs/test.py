e6462470192a6c12ce5063379b507142fe587e19
Rune Aagaard Keena <rune.aagaard.keena@randers.dk>
2024-08-15 13:46:00 +0200

    db test

--- src/jobs/test.py
+++ src/jobs/test.py
@@ -1,12 +1,29 @@
 import logging
-import requests
+import pymssql
+
+from utils.config import SERVER_INVENTORY_DB_NAME, SERVER_INVENTORY_DB_USER, SERVER_INVENTORY_DB_PASS, SERVER_INVENTORY_DB_HOST
+
 logger = logging.getLogger(__name__)
 
+
 def job():
-    logging.info("Test job")
+    logging.info("Test ms db job")
     try:
-        res  = requests.get("https://api.statbank.dk/v1/subjects")
-        logger.info(res)
+        
+        logger.info(SERVER_INVENTORY_DB_HOST)
+        logger.info(type(SERVER_INVENTORY_DB_HOST))
+
+        conn = pymssql.connect(
+            host=SERVER_INVENTORY_DB_HOST,
+            user=SERVER_INVENTORY_DB_USER,
+            password=SERVER_INVENTORY_DB_PASS,
+            database=SERVER_INVENTORY_DB_NAME
+        )
+        cur = conn.cursor()
+
+        cur.execute("SELECT * FROM DiskSpace")
+        all = cur.fetchall()
+        logger.info(all)
     except Exception as e:
         logger.error(e)
         return False
