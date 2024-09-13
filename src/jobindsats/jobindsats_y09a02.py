490640da7a2345b9863c77ff46ad20200b9547e1
nk-randers <nivethan.kandiah@randers.dk>
2024-09-13 13:31:43 +0200

    added jobindsats dataset for y09a02
    added jobindsats dataset for y09a02 - Ledighedsydelse

---
+++ src/jobindsats/jobindsats_y09a02.py
@@ -0,0 +1,75 @@
+import io
+import logging
+from datetime import datetime
+
+import pandas as pd
+from utils.api_requests import APIClient
+from utils.config import JOBINDSATS_API_KEY
+from custom_data_connector import post_data_to_custom_data_connector
+
+logger = logging.getLogger(__name__)
+base_url = "https://api.jobindsats.dk/v2/data/y09a02/json"
+jobindsats_client = APIClient(base_url, JOBINDSATS_API_KEY)
+
+
+def get_jobindsats_ledighedsydelse():
+    try:
+        logger.info("Starting jobindsats Ledighedsydelse")
+        period = dynamic_period()
+        payload = {
+            "area": "*",
+            "period": period,
+            "_kon": [
+                "Kvinder",
+                "Mænd"
+            ],
+            "_oprinda": [
+                "Personer med dansk oprindelse",
+                "Indvandrere fra vestlige lande",
+                "Efterkommere fra vestlige lande",
+                "Indvandrere fra ikke-vestlige lande",
+                "Efterkommere fra ikke-vestlige lande"
+            ]
+        }
+        data = jobindsats_client.make_request(json=payload)
+
+        variables = data[0]['Variables']
+        data = data[0]['Data']
+
+        column_names = [var['Label'] for var in variables]
+
+        df = pd.DataFrame(data, columns=column_names)
+        df['Periode ledighedsydelse'] = df['Periode'].apply(convert_to_datetime)
+
+        df["Periode"] = df["Periode"]
+
+        # Rename column name
+        df.rename(columns={"Area": "Område"}, inplace=True)
+
+        file = io.BytesIO(df.to_csv(index=False, sep=';').encode('utf-8'))
+        filename = "SA" + "JobindsatsY09A02" + ".csv"
+
+        if post_data_to_custom_data_connector(filename, file):
+            logger.info("Successfully updated JobindsatsY09A02")
+            return True
+        else:
+            return False
+
+    except Exception as e:
+        logger.error(f'Error {e}')
+        return False
+
+
+def dynamic_period():
+    current_year = datetime.now().year - 1
+    previous_year = current_year - 1
+    period = [
+        f"{previous_year}Y01", f"{current_year}Y01"
+    ]
+    return period
+
+
+def convert_to_datetime(period_str):  # Convert period string to datetime
+    year = int(period_str[:4])
+    month = int(period_str[5:])
+    return datetime(year, month, 1)
--- src/jobs/jobindsats.py
+++ src/jobs/jobindsats.py
@@ -2,6 +2,7 @@ from jobindsats.jobindsats_y30r21 import get_jobindats_ydelsesgrupper
 from jobindsats.jobindsats_y01a02 import get_jobindsats_dagpenge
 from jobindsats.jobindsats_y07a02 import get_jobindsats_syg_dagpenge
 from jobindsats.jobindsats_y08a02 import get_jobindsats_fleksjob
+from jobindsats.jobindsats_y09a02 import get_jobindsats_ledighedsydelse
 
 import logging
 
@@ -11,6 +12,7 @@ logger = logging.getLogger(__name__)
 def job():
     try:
         logger.info('Starting jobindsats ETL jobs!')
+        get_jobindsats_ledighedsydelse()
         get_jobindsats_fleksjob()
         get_jobindsats_syg_dagpenge()
         get_jobindsats_dagpenge()
