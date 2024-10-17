1df2525efc12fb266c5eaa2449c07da37dd89095
nk-randers <nivethan.kandiah@randers.dk>
2024-10-17 09:54:48 +0200

    adding fixtures to mock dependencis
    adding fixtures to mock dependencies, reducing boilerplate code

--- tests/jobindsats/jobindsats_y35a02_test.py
+++ tests/jobindsats/jobindsats_y35a02_test.py
@@ -1,72 +1,73 @@
+import pytest
 from unittest.mock import patch
 from datetime import datetime
 
 from jobindsats.jobindsats_y35a02 import (
     get_jobindsats_sho,
     dynamic_period,
-    convert_to_datetime
+    convert_to_datetime,
+    period_request
 )
 
 
-def test_get_jobindsats_sho():
+@pytest.fixture
+def mock_dependencies():
     with patch('jobindsats.jobindsats_y35a02.logger') as mock_logger, \
          patch('jobindsats.jobindsats_y35a02.jobindsats_client.make_request') as mock_client_request, \
          patch('jobindsats.jobindsats_y35a02.jobindsats_period_client.make_request') as mock_period_request, \
          patch('jobindsats.jobindsats_y35a02.post_data_to_custom_data_connector') as mock_post_data:
-
-        mock_period_request.return_value = [{'Period': ['2024M01', '2024M02']}]
-        mock_client_request.return_value = [{
-            'Variables': [{'Label': 'Periode'}, {'Label': 'Area'}],
-            'Data': [['2024M01', 'Randers'], ['2024M02', 'Aarhus']]
-        }]
-        mock_post_data.return_value = True
-
-        result = get_jobindsats_sho()
-
-        assert result
-        mock_logger.info.assert_any_call("Starting jobindsats Selvforsørgelses- og hjemrejseydelse samt overgangsydelse")
-        mock_logger.info.assert_any_call("Successfully updated JobindsatsY35A02")
-        mock_post_data.assert_called_once()
-        mock_client_request.assert_called_once()
+        yield {
+            'mock_logger': mock_logger,
+            'mock_client_request': mock_client_request,
+            'mock_period_request': mock_period_request,
+            'mock_post_data': mock_post_data
+        }
 
 
-def test_get_jobindsats_sho_post_data_failure():
-    with patch('jobindsats.jobindsats_y35a02.logger') as mock_logger, \
-         patch('jobindsats.jobindsats_y35a02.jobindsats_client.make_request') as mock_client_request, \
-         patch('jobindsats.jobindsats_y35a02.jobindsats_period_client.make_request') as mock_period_request, \
-         patch('jobindsats.jobindsats_y35a02.post_data_to_custom_data_connector') as mock_post_data:
+def test_get_jobindsats_sho(mock_dependencies):
+    mock_dependencies['mock_period_request'].return_value = [{'Period': ['2024M01', '2024M02']}]
+    mock_dependencies['mock_client_request'].return_value = [{
+        'Variables': [{'Label': 'Periode'}, {'Label': 'Area'}],
+        'Data': [['2024M01', 'Randers'], ['2024M02', 'Aarhus']]
+    }]
+    mock_dependencies['mock_post_data'].return_value = True
 
-        mock_period_request.return_value = [{'Period': ['2024M01', '2024M02']}]
-        mock_client_request.return_value = [{
-            'Variables': [{'Label': 'Periode'}, {'Label': 'Area'}],
-            'Data': [['2024M01', 'Randers'], ['2024M02', 'Aarhus']]
-        }]
-        mock_post_data.return_value = False
+    result = get_jobindsats_sho()
 
-        result = get_jobindsats_sho()
-
-        assert not result
-        mock_logger.info.assert_any_call("Starting jobindsats Selvforsørgelses- og hjemrejseydelse samt overgangsydelse")
-        mock_logger.error.assert_not_called()
-        mock_post_data.assert_called_once()
-        mock_client_request.assert_called_once()
+    assert result
+    mock_dependencies['mock_logger'].info.assert_any_call("Starting jobindsats Selvforsørgelses- og hjemrejseydelse samt overgangsydelse")
+    mock_dependencies['mock_logger'].info.assert_any_call("Successfully updated JobindsatsY35A02")
+    mock_dependencies['mock_post_data'].assert_called_once()
+    mock_dependencies['mock_client_request'].assert_called_once()
 
 
-def test_get_jobindsats_sho_exception():
-    with patch('jobindsats.jobindsats_y35a02.logger') as mock_logger, \
-         patch('jobindsats.jobindsats_y35a02.jobindsats_client.make_request') as mock_client_request, \
-         patch('jobindsats.jobindsats_y35a02.jobindsats_period_client.make_request') as mock_period_request, \
-         patch('jobindsats.jobindsats_y35a02.post_data_to_custom_data_connector') as mock_post_data:
+def test_get_jobindsats_sho_post_data_failure(mock_dependencies):
+    mock_dependencies['mock_period_request'].return_value = [{'Period': ['2024M01', '2024M02']}]
+    mock_dependencies['mock_client_request'].return_value = [{
+        'Variables': [{'Label': 'Periode'}, {'Label': 'Area'}],
+        'Data': [['2024M01', 'Randers'], ['2024M02', 'Aarhus']]
+    }]
+    mock_dependencies['mock_post_data'].return_value = False
 
-        mock_period_request.side_effect = Exception("Test exception")
+    result = get_jobindsats_sho()
 
-        result = get_jobindsats_sho()
+    assert not result
+    mock_dependencies['mock_logger'].info.assert_any_call("Starting jobindsats Selvforsørgelses- og hjemrejseydelse samt overgangsydelse")
+    mock_dependencies['mock_logger'].error.assert_not_called()
+    mock_dependencies['mock_post_data'].assert_called_once()
+    mock_dependencies['mock_client_request'].assert_called_once()
 
-        assert not result
-        mock_logger.info.assert_any_call("Starting jobindsats Selvforsørgelses- og hjemrejseydelse samt overgangsydelse")
-        mock_logger.error.assert_called_once_with('Error Test exception')
-        mock_post_data.assert_not_called()
-        mock_client_request.assert_not_called()
+
+def test_get_jobindsats_sho_exception(mock_dependencies):
+    mock_dependencies['mock_period_request'].side_effect = Exception("Test exception")
+
+    result = get_jobindsats_sho()
+
+    assert not result
+    mock_dependencies['mock_logger'].info.assert_any_call("Starting jobindsats Selvforsørgelses- og hjemrejseydelse samt overgangsydelse")
+    mock_dependencies['mock_logger'].error.assert_called_once_with('Error Test exception')
+    mock_dependencies['mock_post_data'].assert_not_called()
+    mock_dependencies['mock_client_request'].assert_not_called()
 
 
 def test_dynamic_period():
@@ -85,3 +86,11 @@ def test_convert_to_datetime():
     expected_date = datetime(2024, 2, 1)
     result = convert_to_datetime(period_str)
     assert result == expected_date
+
+
+def test_period_request(mock_dependencies):
+    mock_dependencies['mock_period_request'].return_value = [{'Period': ['2024M01', '2024M02', '2024M03', '2023M09', '2024M04']}]
+    result = period_request()
+
+    assert result == '2024M04'
+    mock_dependencies['mock_period_request'].assert_called_once()
