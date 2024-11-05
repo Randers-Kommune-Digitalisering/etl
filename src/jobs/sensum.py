6bca9a491096b1303c3ca703c2a5d186f48c494f
nk-randers <nivethan.kandiah@randers.dk>
2024-11-05 15:09:26 +0100

    Refactor Sensum Job
    Added helper functions and logic to merge df

--- src/jobs/sensum.py
+++ src/jobs/sensum.py
@@ -12,24 +12,22 @@ logger = logging.getLogger(__name__)
 
 
 def job():
-    sftp_client = SFTPClient(SENSUM_IT_SFTP_HOST, SENSUM_IT_SFTP_USER, password=SENSUM_IT_SFTP_PASS)
-    conn = sftp_client.get_connection()
-    sager_files = [f for f in conn.listdir(SENSUM_IT_SFTP_REMOTE_DIR) if fnmatch.fnmatch(f, 'Sager_*.csv')]
-    indsatser_files = [f for f in conn.listdir(SENSUM_IT_SFTP_REMOTE_DIR) if fnmatch.fnmatch(f, 'Indsatser_*.csv')]
-    leverandor_files = [f for f in conn.listdir(SENSUM_IT_SFTP_REMOTE_DIR) if fnmatch.fnmatch(f, 'Leverandør_*.csv')]
-    borger_files = [f for f in conn.listdir(SENSUM_IT_SFTP_REMOTE_DIR) if fnmatch.fnmatch(f, 'Borger_*.csv')]
+    try:
+        sftp_client = SFTPClient(SENSUM_IT_SFTP_HOST, SENSUM_IT_SFTP_USER, password=SENSUM_IT_SFTP_PASS)
+        conn = sftp_client.get_connection()
+        sager_files = get_files(conn, 'Sager_*.csv')
+        indsatser_files = get_files(conn, 'Indsatser_*.csv')
+        leverandor_files = get_files(conn, 'Leverandør_*.csv')
+        borger_files = get_files(conn, 'Borger_*.csv')
 
-    if sager_files and conn:
-        handle_sager_files(sager_files, conn)
+        if leverandor_files:
+            handle_leverandor_files(leverandor_files, conn)
 
-    if leverandor_files and conn:
-        handle_leverandor_files(leverandor_files, conn)
-
-    if indsatser_files and conn:
-        handle_Indsatser_files(indsatser_files, conn)
-
-    if borger_files and conn:
-        handle_borger_files(borger_files, conn)
+        if sager_files and indsatser_files and borger_files:
+            return process_files(sager_files, indsatser_files, borger_files, conn)
+    except Exception as e:
+        logger.error(f"An error occurred: {e}")
+        return False
 
     return False
 
@@ -205,9 +203,67 @@ def handle_borger_files(files, connection):
         return None
 
 
+def merge_df(sager_df, indsatser_df, borger_df):
+    merged_df = pd.merge(indsatser_df, sager_df, on='SagId', how='inner')
+    merged_df = pd.merge(merged_df, borger_df, on='BorgerId', how='inner')
+    result = merged_df.groupby('IndsatsId').agg({
+        'BorgerId': 'nunique',
+        'IndsatsStatus': 'first',
+        'Indsats': 'first',
+        'CPR': 'first',
+        'Fornavn': 'first',
+        'IndsatsStartDato': 'first',
+        'IndsatsSlutDato': 'first',
+        'OprettetDato': 'first',
+        'OpholdsKommune': 'first',
+        'SagNavn': 'first',
+        'SagType': 'first',
+        'Status': 'first',
+        'PrimærAnsvarlig': 'first',
+        'Akut': 'first',
+        'AfslutningsÅrsag': 'first',
+
+    }).reset_index(drop=True)
+
+    result.columns = ['Counter', 'IndsatsStatus', 'Indsats', 'CPR', 'Fornavn', 'IndsatsStartDato', 'IndsatsSlutDato',
+                      'OprettetDato', 'OpholdsKommune', 'SagNavn', 'SagType', 'Status',
+                      'PrimærAnsvarlig', 'Akut', 'AfslutningsÅrsag']
+
+    if pd.isna(result.at[0, 'OprettetDato']):
+        result.at[0, 'OprettetDato'] = pd.Timestamp('1900-01-01')
+
+    result['OprettetDato'] = pd.to_datetime(result['OprettetDato'])
+
+    return result
+
+
 def df_to_csv(df, filename):  # For testing locally purposes
     try:
         df.to_csv(f"{filename}.csv", index=False)
         logger.info(f"DataFrame saved to {filename}.csv")
     except Exception as e:
         logger.error(f"Error saving DataFrame to CSV: {e}")
+
+
+def get_files(conn, pattern):
+    return [f for f in conn.listdir(SENSUM_IT_SFTP_REMOTE_DIR) if fnmatch.fnmatch(f, pattern)]
+
+
+def process_files(sager_files, indsatser_files, borger_files, conn):
+    sager_df = handle_sager_files(sager_files, conn)
+    indsatser_df = handle_Indsatser_files(indsatser_files, conn)
+    borger_df = handle_borger_files(borger_files, conn)
+
+    if sager_df is not None and indsatser_df is not None and borger_df is not None:
+        result = merge_df(sager_df, indsatser_df, borger_df)
+
+        file = io.BytesIO(result.to_csv(index=False, sep=';').encode('utf-8'))
+        filename = "SensumMerged.csv"
+
+        if post_data_to_custom_data_connector(filename, file):
+            logger.info("Successfully updated Sensum Data")
+            return True
+        else:
+            logger.error("Failed to update Sensum Data")
+            return False
+    return False
