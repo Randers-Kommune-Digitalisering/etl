229b67e069084728e9235554b9cca0adbbe275c9
nk-randers <nivethan.kandiah@randers.dk>
2024-11-26 09:19:19 +0100

    retrieve default user

--- src/jobs/asset_management_light.py
+++ src/jobs/asset_management_light.py
@@ -77,6 +77,15 @@ def job():
         else:
             logger.info("No last online data found.")
 
+        # Insert/Update Default User
+        default_user_result = get_default_user(capa_db_client)
+        if default_user_result:
+            for row in default_user_result:
+                logger.info(f"Default User: {row}")
+            update_default_user(sshw_db_client, default_user_result)
+        else:
+            logger.info("No default user data found.")
+
         return True
     except Exception as e:
         logger.error(f"Error in Asset-Management-Light job: {e}")
@@ -304,5 +313,49 @@ def update_last_online(sshw_db_client, data):
             sshw_db_client.execute_sql(sql_command, (last_online, unit_name))
         sshw_db_client.get_connection().commit()
         logger.info("Last Online Data updated successfully in ComputerAssets table.")
+    except Exception as e:
+        logger.error(f"Error updating data in ComputerAssets table: {e}")
+
+
+def get_default_user(capa_db_client):
+    sql_command = """
+    SELECT UNIT.NAME, LGI.VALUE
+    FROM UNIT
+    JOIN LGI ON UNIT.UNITID = LGI.UNITID
+    WHERE LGI.SECTION = 'Default User' AND LGI.NAME = 'User Name'
+    """
+    logger.info(f"Executing SQL command: {sql_command}")
+
+    try:
+        result = capa_db_client.execute_sql(sql_command)
+        if result:
+            filtered_result = []
+            for row in result:
+                unit_name, user_name = row
+                if not (unit_name.startswith('DQ') or unit_name.startswith('AP')):
+                    logger.info(f"Unit Name: {unit_name}, Default User: {user_name}")
+                    filtered_result.append((unit_name, user_name))
+            logger.info(f"Total elements: {len(filtered_result)}")
+            return filtered_result
+        else:
+            logger.error("No results found.")
+            return "NONE"
+    except Exception as e:
+        logger.error(f"Error retrieving default user data: {e}")
+        return None
+
+
+def update_default_user(sshw_db_client, data):
+    sql_command = """
+    UPDATE ComputerAssets
+    SET DefaultUser = %s
+    WHERE UnitName = %s
+    """
+    try:
+        for row in data:
+            unit_name, user_name = row
+            sshw_db_client.execute_sql(sql_command, (user_name, unit_name))
+        sshw_db_client.get_connection().commit()
+        logger.info("Default User Data updated successfully in ComputerAssets table.")
     except Exception as e:
         logger.error(f"Error updating data in ComputerAssets table: {e}")
