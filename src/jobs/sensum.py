71eabc806a4544be3acf2b2475f05cc4d4191dda
nk-randers <nivethan.kandiah@randers.dk>
2025-01-22 13:01:31 +0100

    added data for sensum udfører Administrative forhold

--- src/jobs/sensum.py
+++ src/jobs/sensum.py
@@ -5,6 +5,7 @@ from sensum.sensum_sags_aktiviteter import get_sensum_sags_aktiviteter
 from utils.api_requests import APIClient
 from utils.config import CONFIG_LIBRARY_USER, CONFIG_LIBRARY_PASS, CONFIG_LIBRARY_URL, SENSUM_CONFIG_LIBRARY_PATH
 from sensum.sensum_ydelse import get_sensum_ydelse
+from sensum.sensum_administrative_forhold import get_sensum_adminstrative_forhold
 
 logger = logging.getLogger(__name__)
 
@@ -33,6 +34,7 @@ def job():
         results.append(get_sensum_sags_aktiviteter())
         results.append(get_sensum_data())
         results.append(get_sensum_ydelse())
+        results.append(get_sensum_adminstrative_forhold())
         return all(results)
     except Exception as e:
         logger.error(f'An error occurred: {e}')
---
+++ src/sensum/sensum_administrative_forhold.py
@@ -0,0 +1,75 @@
+import io
+import logging
+import pandas as pd
+from custom_data_connector import post_data_to_custom_data_connector
+from utils.sftp_connection import get_sftp_client
+from utils.config import SENSUM_IT_SFTP_REMOTE_SUBFOLDER_DIR
+from sensum.sensum import handle_sub_folder_files, get_subfolders
+
+logger = logging.getLogger(__name__)
+
+sftp_client = get_sftp_client()
+
+
+def get_sensum_adminstrative_forhold():
+    try:
+        logger.info('Starting Sensum Udfører Data: Administrative forhold')
+        conn = sftp_client.get_connection()
+        subfolders = get_subfolders()
+
+        borger_information_df = handle_sub_folder_files(subfolders, conn, 'Borger_information_*.csv', SENSUM_IT_SFTP_REMOTE_SUBFOLDER_DIR)
+        adminstrative_forhold_df = handle_sub_folder_files(subfolders, conn, 'Administrative_*.csv', SENSUM_IT_SFTP_REMOTE_SUBFOLDER_DIR)
+
+        if borger_information_df is not None and adminstrative_forhold_df is not None:
+            return process_files(borger_information_df, adminstrative_forhold_df)
+    except Exception as e:
+        logger.error(f"An error occurred: {e}")
+        return False
+
+    return False
+
+
+def merge_df(borger_information_df, adminstrative_forhold_df):
+    merged_df = pd.merge(borger_information_df, adminstrative_forhold_df, on='BorgerId', how='inner')
+
+    result = merged_df.groupby('BorgerId').agg({
+        'Fornavn': 'first',
+        'Efternavn': 'first',
+        'CPR': 'first',
+        'Afdeling': 'first',
+        'IndskrivelseDato': 'first',
+        'UdskrivelsesDato': 'first',
+        'HandleKommune': 'first',
+        'BetalingsKommune': 'first',
+        'ParagrafType': 'first',
+        'IndskrivningsType': 'first',
+        'PrimærAdresse': 'first',
+        'PrimærPostNr': 'first',
+        'PrimærBy': 'first'
+    }).reset_index(drop=True)
+
+    result.columns = ['Fornavn', 'Efternavn', 'CPR', 'Afdeling', 'IndskrivelseDato', 'UdskrivelsesDato', 'HandleKommune',
+                      'BetalingsKommune', 'ParagrafType', 'IndskrivningsType', 'PrimærAdresse',
+                      'PrimærPostNr', 'PrimærBy']
+
+    if pd.isna(result.at[0, 'UdskrivelsesDato']):
+        result.at[0, 'UdskrivelsesDato'] = pd.Timestamp('1900-01-01')
+    if pd.isna(result.at[0, 'HandleKommune']):
+        result.at[0, 'HandleKommune'] = 'Ikke angivet'
+    if pd.isna(result.at[0, 'BetalingsKommune']):
+        result.at[0, 'BetalingsKommune'] = 'Ikke angivet'
+
+    return result
+
+
+def process_files(borger_information_df, adminstrative_forhold_df):
+    result = merge_df(borger_information_df, adminstrative_forhold_df)
+
+    file = io.BytesIO(result.to_csv(index=False, sep=';').encode('utf-8'))
+    filename = "SA" + "SensumUdførerAdminstrativeForhold" + ".csv"
+    if post_data_to_custom_data_connector(filename, file):
+        logger.info("Successfully updated Sensum Administrative forhold Data")
+        return True
+    else:
+        logger.error("Failed to update Sensum Administrative forhold Data")
+        return False
