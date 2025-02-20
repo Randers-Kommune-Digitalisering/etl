d4a70b84ca8669ebb3301bbc28829faa08994b1d
Rune Aagaard Keena <rune.aagaard.keena@randers.dk>
2025-02-20 12:03:37 +0100

    new frondesk data pull job - upload to randers test sftp - no longer using custom-data-connector

--- src/jobs/borgerservice_data_pull.py
+++ src/jobs/borgerservice_data_pull.py
@@ -3,26 +3,38 @@ import pandas as pd
 import logging
 import io
 
-from utils.config import FRONTDESK_DB_USER, FRONTDESK_DB_PASS, FRONTDESK_DB_HOST, FRONTDESK_DB_DATABASE
-from custom_data_connector import post_data_to_custom_data_connector
 
-logging.getLogger("prophet.plot").disabled = True
+from utils.stfp import SFTPClient
+from utils.config import FRONTDESK_DB_USER, FRONTDESK_DB_PASS, FRONTDESK_DB_HOST, FRONTDESK_DB_DATABASE, RANDERS_TEST_SFTP_HOST, RANDERS_TEST_SFTP_USER, RANDERS_TEST_SFTP_PASS, FRONTDESK_DIR
+
+
 logger = logging.getLogger(__name__)
 
 
 def job():
-    logger.info("Initializing frontdesk borgerservice data pull job")
+    try:
+        logger.info("Initializing frontdesk borgerservice data pull job")
 
-    workdata = connectToFrontdeskDBtables()
+        if None not in (RANDERS_TEST_SFTP_HOST, RANDERS_TEST_SFTP_USER, RANDERS_TEST_SFTP_PASS):
+            workdata = connectToFrontdeskDBtables()
 
-    file = io.BytesIO(workdata.to_csv(index=False, sep=';').encode('utf-8'))
-    filename = "SA" + "FrontdeskBorgerserviceTables" + ".csv"
+            file = io.BytesIO(workdata.to_csv(index=False, sep=';').encode('utf-8'))
+            filename = "FrontdeskBorgerserviceTables.csv"
 
-    if post_data_to_custom_data_connector(filename, file):
-        logger.info("Updated Frontdesk Borgerservice data successfully")
+            sftp_client = SFTPClient(RANDERS_TEST_SFTP_HOST, RANDERS_TEST_SFTP_USER, RANDERS_TEST_SFTP_PASS)
+
+            with sftp_client.get_connection() as conn:
+                conn.putfo(file, f"{FRONTDESK_DIR}/{filename}")
+
+            logger.info(f"{filename} written to SFTP server")
+
+        else:
+            logger.warning("SFTP credentials not set - skipping data pull job")
+
         return True
-    else:
-        logger.error("Failed to update Frontdesk Borgerservice data")
+
+    except Exception as e:
+        logger.error(e)
         return False
 
 
