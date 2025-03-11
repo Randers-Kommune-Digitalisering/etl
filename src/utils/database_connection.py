f28e1b6fb527e1e4cdd08d90dd9fc0ae3682a405
nk-randers <nivethan.kandiah@randers.dk>
2025-03-11 15:35:08 +0100

    add get_db_zylinc function

--- src/utils/database_connection.py
+++ src/utils/database_connection.py
@@ -1,5 +1,9 @@
 from utils.database_client import DatabaseClient
-from utils.config import SENSUM_DB_DATABASE, SENSUM_DB_USER, SENSUM_DB_PASS, SENSUM_DB_HOST, SENSUM_DB_PORT, POSTGRES_DB_USER, POSTGRES_DB_PASS, POSTGRES_DB_HOST, POSTGRES_DB_DATABASE, POSTGRES_DB_PORT
+from utils.config import (
+    SENSUM_DB_DATABASE, SENSUM_DB_USER, SENSUM_DB_PASS, SENSUM_DB_HOST, SENSUM_DB_PORT,
+    POSTGRES_DB_USER, POSTGRES_DB_PASS, POSTGRES_DB_HOST, POSTGRES_DB_DATABASE, POSTGRES_DB_PORT,
+    ZYLINC_POSTGRES_DB_DATABASE, ZYLINC_POSTGRES_DB_USER, ZYLINC_POSTGRES_DB_PASS, ZYLINC_POSTGRES_DB_HOST, ZYLINC_POSTGRES_DB_PORT
+)
 
 
 def get_db_client():
@@ -21,4 +25,15 @@ def get_db_frontdesk():
         password=POSTGRES_DB_PASS,
         host=POSTGRES_DB_HOST,
         port=POSTGRES_DB_PORT
+    )
+
+
+def get_db_zylinc():
+    return DatabaseClient(
+        db_type='postgresql',
+        database=ZYLINC_POSTGRES_DB_DATABASE,
+        username=ZYLINC_POSTGRES_DB_USER,
+        password=ZYLINC_POSTGRES_DB_PASS,
+        host=ZYLINC_POSTGRES_DB_HOST,
+        port=ZYLINC_POSTGRES_DB_PORT
     )
