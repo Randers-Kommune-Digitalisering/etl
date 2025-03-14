3c6a2630863db67c200094896b31aabe0bc117a9
nk-randers <nivethan.kandiah@randers.dk>
2025-03-14 09:17:54 +0100

    close db connection properly

--- src/zylinc/zylinc.py
+++ src/zylinc/zylinc.py
@@ -5,6 +5,7 @@ logger = logging.getLogger(__name__)
 
 
 def insert_data_to_db(db_client, data):
+    connection = None
     try:
         logger.info("Ensuring database exists")
         db_client.ensure_database_exists()
@@ -19,12 +20,14 @@ def insert_data_to_db(db_client, data):
             logger.info("DataFrame created, inserting data into PostgreSQL database")
             df.to_sql('zylinc', con=connection, if_exists='replace', index=False, chunksize=1000)
             logger.info("Data successfully inserted into PostgreSQL database")
-            connection.close()
-            logger.info("Database connection closed")
         else:
             raise Exception("Failed to get database connection")
     except Exception as e:
         logger.error(f"Error inserting data into PostgreSQL: {e}")
+    finally:
+        if connection:
+            connection.close()
+            logger.info("Database connection closed")
 
 
 def query_elasticsearch(es_client, scroll_size, body):
