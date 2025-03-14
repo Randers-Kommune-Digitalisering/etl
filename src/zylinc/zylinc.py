bd4f7ed62d82d31b4ae884b779a4afacf3b3f1f2
nk-randers <nivethan.kandiah@randers.dk>
2025-03-14 08:44:54 +0100

    add chunksize in order to avoid locks

--- src/zylinc/zylinc.py
+++ src/zylinc/zylinc.py
@@ -17,7 +17,7 @@ def insert_data_to_db(db_client, data):
             logger.info("Converting data to DataFrame")
             df = pd.DataFrame(data)
             logger.info("DataFrame created, inserting data into PostgreSQL database")
-            df.to_sql('zylinc', con=connection, if_exists='replace', index=False)
+            df.to_sql('zylinc', con=connection, if_exists='replace', index=False, chunksize=1000)
             logger.info("Data successfully inserted into PostgreSQL database")
             connection.close()
             logger.info("Database connection closed")
