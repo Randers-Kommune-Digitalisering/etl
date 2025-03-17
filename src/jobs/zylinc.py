eee5787351691dcb3949eec7d3e3a2b9f609b2b4
nk-randers <nivethan.kandiah@randers.dk>
2025-03-17 09:07:09 +0100

    update zylinc.py job

--- src/jobs/zylinc.py
+++ src/jobs/zylinc.py
@@ -1,7 +1,8 @@
 import logging
+import pandas as pd
 from utils.elastic_search_client import ElasticSearchClient
 from utils.config import ELASTICSEARCH_HOST, ELASTICSEARCH_PORT, ELASTICSEARCH_USER, ELASTICSEARCH_PASS
-from zylinc.zylinc import insert_data_to_db, query_elasticsearch
+from zylinc.zylinc import query_elasticsearch
 from utils.database_connection import get_db_zylinc
 
 logger = logging.getLogger(__name__)
@@ -45,7 +46,25 @@ def job():
         try:
             data_to_insert = query_elasticsearch(es_client, scroll_size, body)
 
-            insert_data_to_db(db_client, data_to_insert)
+            try:
+                logger.info("Ensuring database exists")
+                db_client.ensure_database_exists()
+                logger.info("Database exists or created successfully")
+
+                logger.info("Attempting to get database connection")
+                with db_client.get_connection() as connection:
+                    if connection:
+                        logger.info("Database connection established")
+                        logger.info("Converting data to DataFrame")
+                        df = pd.DataFrame(data_to_insert)
+                        logger.info("DataFrame created, inserting data into PostgreSQL database")
+                        df.to_sql('zylinc', con=connection, if_exists='replace', index=False, chunksize=1000)
+                        logger.info("Data successfully inserted into PostgreSQL database")
+                    else:
+                        raise Exception("Failed to get database connection")
+            except Exception as e:
+                logger.error(f"Error inserting data into PostgreSQL: {e}")
+                return False
 
             logger.info("Data saved to PostgreSQL database")
             return True
--- src/zylinc/zylinc.py
+++ src/zylinc/zylinc.py
@@ -1,33 +1,6 @@
-import pandas as pd
 import logging
 
 logger = logging.getLogger(__name__)
-
-
-def insert_data_to_db(db_client, data):
-    connection = None
-    try:
-        logger.info("Ensuring database exists")
-        db_client.ensure_database_exists()
-        logger.info("Database exists or created successfully")
-
-        logger.info("Attempting to get database connection")
-        connection = db_client.get_connection()
-        if connection:
-            logger.info("Database connection established")
-            logger.info("Converting data to DataFrame")
-            df = pd.DataFrame(data)
-            logger.info("DataFrame created, inserting data into PostgreSQL database")
-            df.to_sql('zylinc', con=connection, if_exists='replace', index=False, chunksize=1000)
-            logger.info("Data successfully inserted into PostgreSQL database")
-        else:
-            raise Exception("Failed to get database connection")
-    except Exception as e:
-        logger.error(f"Error inserting data into PostgreSQL: {e}")
-    finally:
-        if connection:
-            connection.close()
-            logger.info("Database connection closed")
 
 
 def query_elasticsearch(es_client, scroll_size, body):
