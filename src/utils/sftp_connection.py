36572fa07c446000130c6b4c6198cd11ec4b8793
nk-randers <nivethan.kandiah@randers.dk>
2025-03-26 08:50:55 +0100

    added get_asset_sftp_client

--- src/utils/sftp_connection.py
+++ src/utils/sftp_connection.py
@@ -1,6 +1,18 @@
-from utils.config import SENSUM_IT_SFTP_HOST, SENSUM_IT_SFTP_USER, SENSUM_IT_SFTP_PASS
+from utils.config import SENSUM_IT_SFTP_HOST, SENSUM_IT_SFTP_USER, SENSUM_IT_SFTP_PASS, ASSET_SFTP_HOST, ASSET_SFTP_USER, ASSET_SFTP_PASS
 from utils.stfp import SFTPClient
 
 
 def get_sftp_client():
     return SFTPClient(SENSUM_IT_SFTP_HOST, SENSUM_IT_SFTP_USER, password=SENSUM_IT_SFTP_PASS)
+
+
+def get_asset_sftp_client():
+    return SFTPClient(ASSET_SFTP_HOST, ASSET_SFTP_USER, password=ASSET_SFTP_PASS)
+
+
+def download_csv_from_asset_sftp(sftp_file_path):
+    sftp_client = get_asset_sftp_client()
+    with sftp_client.get_connection() as conn:
+        with conn.open(sftp_file_path, 'r') as file:
+            csv_data = file.read().decode('utf-8')
+    return csv_data
