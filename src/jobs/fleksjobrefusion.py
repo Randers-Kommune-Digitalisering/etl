d008029b08c3192df190759229a5850f423f4bb4
nk-randers <nivethan.kandiah@randers.dk>
2025-08-04 13:52:44 +0200

    send excel file with failed tjenestenummer via mailservice

--- src/jobs/fleksjobrefusion.py
+++ src/jobs/fleksjobrefusion.py
@@ -7,8 +7,13 @@ from sd_fleksjobrefusion.fleksjobrefusion_data import (
     excel_to_sd_fleksjobrefusion_config,
 )
 from selenium import webdriver
+from mail import send_mail_with_attachment
+from utils.utils import df_to_excel_bytes
+import datetime
 from selenium.webdriver.chrome.options import Options
 from utils.sftp_connection import get_sd_sftp_client
+import pandas as pd
+from utils.config import SD_FLEKSJOBREFUSION_TO_MAIL, SD_FLEKSJOBREFUSION_FROM_MAIL
 logger = logging.getLogger(__name__)
 
 options = Options()
@@ -41,12 +46,32 @@ def job():
             beloeb = person["beloeb"]
             loenart = person["loenart"]
             if not process_person(driver, tjenestenummer, institution, beloeb, loenart):
-                error.append((tjenestenummer, institution, beloeb, loenart))
+                error.append({
+                    "Tjenestenummer": tjenestenummer,
+                    "Institution": institution,
+                    "Beløb": beloeb,
+                    "Lønart": loenart
+                })
 
         if error:
             logger.info("The following persons failed:")
-            for tjenestenummer, institution, beloeb, loenart in error:
-                logger.error(f"- {tjenestenummer} ({institution}): {beloeb} - {loenart}")
+            for e in error:
+                logger.error(f"- {e['Tjenestenummer']} ({e['Institution']}): {e['Beløb']} - {e['Lønart']}")
+
+            df_error = pd.DataFrame(error, columns=["Tjenestenummer", "Institution", "Beløb", "Lønart"])
+            today = datetime.date.today()
+            excel_file = df_to_excel_bytes(df_error)
+
+            send_mail_with_attachment(
+                to_mail=SD_FLEKSJOBREFUSION_TO_MAIL,
+                from_mail=SD_FLEKSJOBREFUSION_FROM_MAIL,
+                title=f'Fleksjob Refusion fejl for {today.strftime("%d.%m.%Y")}',
+                body='Liste af personer med fejl er vedhæftet.',
+                file_name='FleksjobRefusionFejl.xlsx',
+                file_bytes=excel_file
+            )
+
+            logger.error("Some persons had errors. Check the email for details")
             return False
         else:
             logger.info("All persons were processed correctly.")
