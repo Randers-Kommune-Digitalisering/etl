fb93c7a6944fe535db6089d49eb015b32495b848
Rune Aagaard Keena <rune.aagaard.keena@randers.dk>
2025-08-27 14:54:56 +0200

    handle start dates for 3/midlertidig ude af løn

--- src/sd_delta.py
+++ src/sd_delta.py
@@ -137,8 +137,16 @@ def get_employments_with_changes_df(excluded_institutions_df, excluded_departmen
                                                     None
                                                 )
                                                 if row:
-                                                    # Update existing row with new end date
-                                                    row['Slutdato'] = ".".join(reversed(employee['end_date'].split("-")))
+                                                    # Update existing row with new start and end dates to cover entire period
+                                                    existing_start = datetime.strptime(row['Startdato'], "%d.%m.%Y")
+                                                    existing_end = datetime.strptime(row['Slutdato'], "%d.%m.%Y")
+                                                    
+                                                    new_start_string = ".".join(reversed(old_start_date.split("-"))) if old_start_date else ".".join(reversed(employee['start_date'].split("-")))
+                                                    new_start = datetime.strptime(new_start_string), "%d.%m.%Y")
+                                                    new_end = datetime.strptime(".".join(reversed(employee['end_date'].split("-"))), "%d.%m.%Y")
+                                                    
+                                                    row['Startdato'] = (new_start if new_start < existing_start else existing_start).strftime("%d.%m.%Y")
+                                                    row['Slutdato'] = (new_end if new_end > existing_end else existing_end).strftime("%d.%m.%Y")
                                                     continue
                                                 else:
                                                     # Set to '1' / 'Aktiv'- to avoid Delta setting employee to inactive
