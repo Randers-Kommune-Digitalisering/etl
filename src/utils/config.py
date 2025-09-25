1641a2b4993c4b915be16216058e373660f4866f
Nivethan Kandiah <nivethan.kandiah@randers.dk>
2025-09-25 12:14:51 +0200

    Update config.p

--- src/utils/config.py
+++ src/utils/config.py
@@ -180,5 +180,8 @@ ASSET_SFTP_DEVICE_FILE_PATH = 'Computers.csv'
 ASSET_SFTP_COMM2IG_HISTORICAL_FILE_PATH = '/Historisk_data/Historisk_Comm2ig.csv'
 ASSET_SFTP_EAN_ATEA_FILE_PATH = '/Historisk_data/ATEA kundenumre med EAN.xlsx'
 ASSET_SFTP_AFDELINGS_EAN_DELTA_FILE_PATH = '/Historisk_data/Afdeling_EAN_Delta.xlsx'
-ASSET_MAIL_TO = os.environ['ASSET_MAIL_TO'].rstrip()
-ASSET_MAIL_FROM = os.environ['ASSET_MAIL_FROM'].rstrip()
+
+TOPDESK_API_USERNAME = os.getenv("TOPDESK_API_USERNAME")
+TOPDESK_API_PASSWORD = os.getenv("TOPDESK_API_PASSWORD")
+TOPDESK_API_URL = os.getenv("TOPDESK_API_URL")
+TOPDESK_ASSET_FILENAME = 'Assets_With_EAN_Updated.csv'
